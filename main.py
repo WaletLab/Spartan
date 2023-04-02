72ffceb77f6bc3c6e3b7c898b255c627cc70ce2a
dannyx-hub <daro322.dp@gmail.com>
2023-04-02 23:37:39 +0200

    v0.0.2 changelog: - change sys.argv to argparse - clean some code - add mode to return only ports with known services - add os detection - add tabulate to improve output - clean unused imports

--- main.py
+++ main.py
@@ -2,51 +2,57 @@ from art import tprint
 from helpers import color
 import sys
 import time
+import argparse
+from tabulate import tabulate
 from port_scan import Scanner
+debug = False
+if debug is not True:
+    tprint("Spartan")
+    print(color.ITALIC + "\t We make shit safe again \n" + color.STOP_ITALIC)
+    print("v0.0.2 created by " + color.BOLD + "dannyx-hub\n" + color.END)
+parser = argparse.ArgumentParser(description="Tools to beat pussy scanners")
+parser.add_argument("--host", type=str, help="ip address")
+parser.add_argument("--port", type=str, help="port to scan if all print -a if range print example: 1-20")
+parser.add_argument("--mode", type=str, help="scan mode")
+parser.add_argument("--only_known_service", help="return only port with known services", action='store_true')
+args = parser.parse_args()
 
+sc = Scanner()
+hostname = args.host
+port = args.port
+port_range = None
+udp = None
 
-
-tprint("Welcome to Spartan")
-print(color.ITALIC+"\t We make shit safe again \n"+color.STOP_ITALIC)
-print("v0.0.1 created by "+color.BOLD+"dannyx-hub\n"+color.END)
-if len(sys.argv) <= 1:
-    print(color.RED+"ERROR:"+color.END+" no options selected - write --help or -h to check all options")
+# scan
+print("*" * 50)
+print(f"\tPort Scanner \nSpartan start to check ports on {hostname}")
+print("*" * 50)
+if args.mode == "u":
+    print(" [*] udp mode activate [*] ")
+    udp = True
+if args.port == "a":
+    print(" [*] beat some rustscan mode activate [*] ")
+    port = None
 else:
-    sys_len = len(sys.argv)
-    if sys.argv[1] == "--scanner":
-        # TODO port_range jest do przerobienia bo teraz ify sie pierdola
-        sc = Scanner()
+    if args.port.find("-") != -1:
         port = None
-        port_range = None
-        print("*" * 30)
-        print("\tPort Scanner")
-        print("*" * 30)
-        hostname = sys.argv[2]
-        if sys_len > 3:
-            if sys.argv[3] == "-p":
-                port = int(sys.argv[4])
-            elif sys.argv[3] == "-pr":
-                port_range = {'start': int(sys.argv[4]), "stop": int(sys.argv[5])}
-
-        result = sc.scan(hostname=hostname, port=port, port_range=port_range)
-        if result:
-            print(color.GREEN + "Result for {}:".format(hostname) + color.END)
-            print("\t PORT  STATUS")
-            for x in result:
-                print("\t  {}  {}".format(x['port'], x['status']))
-        else:
-            print(color.RED + "Result for {}: no open ports founds".format(hostname) + color.END)
-
-    elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
-        print("""\t
-        --scanner or -p: port scaner
-        --services or -s: service scanner
-        --shodan or -d: shodan info getters
-        --remote or -r: remote shell exploits
-        --help or -h: help
-        """)
+        ports = args.port.split("-")
+        port_range = {"start": int(ports[0]), "stop": int(ports[1])}
+        print(port_range)
     else:
-        print(color.RED+"ERROR:"+color.END+" invalid command - write --help or -h to check all options")
-
-    print("\nProgram end in: "+color.BOLD+"{}".format(time.process_time())+color.END)
-    sys.exit("done")
+        port = int(args.port)
+result = sc.scan(hostname=hostname, port=port, port_range=port_range, udp=udp)
+if result:
+    print(color.GREEN + "Result for {}:".format(hostname) + color.END)
+    print(f"found: {len(result)}")
+    header = ['TYPE', 'PORT', 'STATUS', 'SERVICE', 'INFO']
+    if args.only_known_service:
+        table_data = [[x['type'], x['port'], x['status'], x['service'], x['info']]
+                      for x in result if x['service'] is not None]
+    else:
+        table_data = [[x['type'], x['port'], x['status'], x['service'], x['info']] for x in result]
+    print(tabulate(table_data, headers=header))
+else:
+    print(color.RED + "Result for {}: no open ports founds".format(hostname) + color.END)
+print("\nProgram end in: " + color.BOLD + "{}".format(time.process_time()) + color.END)
+sys.exit(0)
--- port_scan.py
+++ port_scan.py
@@ -1,42 +1,128 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
 import os
+import re
+
+
+def get_service(port,type):
+    try:
+        result = socket.getservbyport(port,type)
+        if result:
+            return result
+    except OSError:
+        pass
+
+
+def split_port_lists(lst, chunk_size):
+    result = [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+    return result
+
+
+def check_html_response(data):
+    # server info
+    server_info = re.search(b"Server: (.+?)\r\n", data)
+    # Extract date information
+    date_pattern = re.compile(rb'Date: (.+?)\r\n')
+    date_match = date_pattern.search(data)
+    # Extract content type information
+    content_type_pattern = re.compile(rb'Content-Type: (.+?)\r\n')
+    content_type_match = content_type_pattern.search(data)
+    # Extract transfer encoding information
+    transfer_encoding_pattern = re.compile(rb'Transfer-Encoding: (.+?)\r\n')
+    transfer_encoding_match = transfer_encoding_pattern.search(data)
+    # Extract accept ranges information
+    accept_ranges_pattern = re.compile(rb'Accept-Ranges: (.+?)\r\n')
+    accept_ranges_match = accept_ranges_pattern.search(data)
+    # Extract vary information
+    vary_pattern = re.compile(rb'Vary: (.+?)\r\n')
+    vary_match = vary_pattern.search(data)
+    return {"server": server_info.group(1).decode(),
+            "date": date_match.group(1).decode(),
+            "content": content_type_match.group(1).decode(),
+            "transfer": transfer_encoding_match.group(1).decode(),
+            "accpet-range": accept_ranges_match.group(1).decode(),
+            "vary": vary_match.group(1).decode()
+            }
+
 
 class Scanner:
 
     def __init__(self):
-        self.executor = ThreadPoolExecutor(max_workers=max(32, os.cpu_count() + 4))
-        print(max(32, os.cpu_count() + 4))
         self.scan_list = []
-    def split_port_lists(self, lst, chunk_size):
-        result = [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-        return result
-    def scan(self, hostname, port=None, port_range=None):
-        if hostname:
-            if port:
-                self._scan(hostname, port)
-            elif port_range:
-                lst = [x for x in range(port_range['start'], port_range['stop'])]
-                chunk_size = 10
-                ports = self.split_port_lists(lst, chunk_size)
-                for port in ports:
-                    self.executor.map(lambda p: self._scan(hostname, p), port)
-            else:
-                lst = [x for x in range(1, 65535)]
-                chunk_size = 10000
-                ports = self.split_port_lists(lst, chunk_size)
-                for port in ports:
-                    print("sprawdzam paczke:")
-                    print(port)
-                    self.executor.map(lambda p: self._scan(hostname, p), port)
-            return self.scan_list
-        else:
-            return ValueError("no hostname")
+        self.stop = False
 
-    def _scan(self, hostname, port):
+    def scan(self, hostname, port=None, port_range=None, udp=True):
+        with ThreadPoolExecutor(max_workers=max(32, os.cpu_count() + 4)) as executor:
+            if hostname:
+                if port is not None:
+                    self._scan(hostname, port, udp)
+                elif port_range:
+                    lst = [x for x in range(int(port_range['start']), int(port_range['stop']))]
+                    chunk_size = 10
+                    ports = split_port_lists(lst, chunk_size)
+                    for single in ports:
+                        executor.map(lambda p: self._scan(hostname, p, udp), single)
+                else:
+                    lst = [x for x in range(1, 65535)]
+                    chunk_size = 100
+                    ports = split_port_lists(lst, chunk_size)
+                    for port in ports:
+                        executor.map(lambda p: self._scan(hostname, p, udp), port)
+                self.stop = True
+                # for skan in self.scan_list:
+                #     get_service(scan=skan)
+                executor.shutdown(wait=True)
+                return self.scan_list
+            else:
+                return ValueError("no hostname")
+
+    def _scan(self, hostname, port, udp):
+        if self.stop:
+            return
+        temp_dict_tcp = {"type": None, "port": None, "status": None,
+                         "service": None, "info": None}
+        temp_dict_udp = {"type": None, "port": None, "status": None,
+                         "service": None, "info": None}
+        sys_os = None
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            # try:
+            s.settimeout(1)
             target = socket.gethostbyname(hostname)
             result = s.connect_ex((target, port))
             if result == 0:
-                self.scan_list.append({"port": port, "status": "open"})
+                temp_dict_tcp['type'] = "tcp"
+                temp_dict_tcp['port'] = port
+                temp_dict_tcp['status'] = "OPEN"
+                temp_dict_tcp['service'] = get_service(port, "tcp")
+                if port == 80:
+                    s.sendall(b"GET / HTTP/1.1\r\nHost: " + hostname.encode() + b"\r\n\r\n")
+                    html_data = s.recv(1024)
+                    parse_html_data = check_html_response(html_data)
+                    strings = ""
+                    for key in parse_html_data.keys():
+                        strings += f"{key}: {parse_html_data[key]} \n"
+                    temp_dict_tcp['info'] = strings
+                try:
+                    if port != 80:
+                        data = s.recv(1024)
+                        if data:
+                            temp_dict_tcp['info'] = data.decode('utf-8')
+                except TimeoutError:
+                    temp_dict_tcp['info'] = None
+                self.scan_list.append(temp_dict_tcp)
+        if udp:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as u:
+                u.settimeout(1)
+                target = socket.gethostbyname(hostname)
+                result = u.connect_ex((target, port))
+                if result == 0:
+                    temp_dict_udp['type'] = "udp"
+                    temp_dict_udp['port'] = port
+                    temp_dict_udp['status'] = "OPEN"
+                    temp_dict_tcp['service'] = get_service(port, "tcp")
+                    try:
+                        data = u.recv(1024)
+                        if data:
+                            temp_dict_udp['info'] = data.decode('utf-8')
+                    except TimeoutError:
+                        temp_dict_udp['info'] = None
+                    self.scan_list.append(temp_dict_udp)
