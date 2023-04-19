d9e64b37e2006ce2f9bab2fa3176e2a9ebc3dfd2
dannyx-hub <daro322.dp@gmail.com>
2023-04-19 21:09:21 +0200

    im not fast im more precisely

--- port_scan.py
+++ port_scan.py
@@ -1,8 +1,7 @@
 import socket
 import time
-from concurrent.futures import ThreadPoolExecutor
-import os
-# import re
+from contextlib import contextmanager
+import asyncio
 
 
 def get_service(port, typ):
@@ -38,82 +37,53 @@ def check_html_response(data):
 
 
 class Scanner:
-
-    def __init__(self):
+    def __init__(self, host, ports, timeout=1):
+        self.ports = ports
+        self.target = host
+        self.timeout = timeout
+        self.total_time = float()
+        self._observers = list()
         self.scan_list = []
-        self.stop = False
+        self._loop = asyncio.new_event_loop()
 
-    def scan(self, hostname, port=None, port_range=None, udp=True):
-        with ThreadPoolExecutor(max_workers=max(32, os.cpu_count() + 4)) as executor:
-            if hostname:
-                if port is not None:
-                    self._scan(hostname, port, udp)
-                elif port_range:
-                    lst = [x for x in range(int(port_range['start']), int(port_range['stop']))]
-                    chunk_size = 10
-                    ports = split_port_lists(lst, chunk_size)
-                    for single in ports:
-                        executor.map(lambda p: self._scan(hostname, p, udp), single)
-                else:
-                    lst = [x for x in range(1, 65535)]
-                    chunk_size = 100
-                    ports = split_port_lists(lst, chunk_size)
-                    for port in ports:
-                        executor.map(lambda p: self._scan(hostname, p, udp), port)
-                self.stop = True
-                executor.shutdown(wait=True)
-                print(self.scan_list)
-                for scan in self.scan_list:
-                    scan['service'] = get_service(scan['port'], scan['type'])
-                return self.scan_list
-            else:
-                return ValueError("no hostname")
+    @property
+    def _scan_tasks(self):
+        """setup a corutine for pair target-port"""
+        return [self._scan_target_port(target, port) for port in self.ports for target in [self.target]]
 
-    def _scan(self, hostname, port, udp):
-        if self.stop:
-            return
-        temp_dict_tcp = {"type": None, "port": None, "status": None,
-                         "service": None, "info": None}
-        temp_dict_udp = {"type": None, "port": None, "status": None,
-                         "service": None, "info": None}
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.8)
-            target = socket.gethostbyname(hostname)
-            result = s.connect_ex((target, port))
-            if result == 0:
-                temp_dict_tcp['type'] = "tcp"
-                temp_dict_tcp['port'] = port
-                temp_dict_tcp['status'] = "OPEN"
-                if port == 80:
-                    s.sendall(b"GET / HTTP/1.1\r\nHost: " + hostname.encode() + b"\r\n\r\n")
-                    html_data = s.recv(1024)
-                    parse_html_data = check_html_response(html_data)
-                    strings = ""
-                    if parse_html_data:
-                        for key in parse_html_data.keys():
-                            strings += f"{key}: {parse_html_data[key]} \n"
-                        temp_dict_tcp['info'] = strings
-                try:
-                    if port != 80:
-                        data = s.recv(1024)
-                        if data:
-                            temp_dict_tcp['info'] = data.decode('utf-8')
-                except TimeoutError:
-                    temp_dict_tcp['info'] = None
-                self.scan_list.append(temp_dict_tcp)
-        if udp:
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as u:
-                u.settimeout(1)
-                target = socket.gethostbyname(hostname)
-                result = u.connect_ex((target, port))
-                if result == 0:
-                    temp_dict_udp['type'] = "udp"
-                    temp_dict_udp['port'] = port
-                    temp_dict_udp['status'] = "OPEN"
-                    try:
-                        data = u.recv(1024)
-                        if data:
-                            temp_dict_udp['info'] = data.decode('utf-8')
-                    except TimeoutError:
-                        temp_dict_udp['info'] = None
-                    self.scan_list.append(temp_dict_udp)
+    @contextmanager
+    def _timer(self):
+        start_time = time.perf_counter()
+        yield
+        self.total_time = time.perf_counter() - start_time
+
+    def register(self, observer):
+        self._observers.append(observer)
+
+    async def _notify(self):
+        [asyncio.create_task(observer.update()) for observer in self._observers]
+
+    async def _scan_target_port(self, addr, port):
+        temp_dict = {'type': "TCP", 'port': port, 'status': None, 'service': None, 'info': None}
+        try:
+            await asyncio.wait_for(
+                asyncio.open_connection(addr, port), timeout=self.timeout)
+
+            temp_dict['status'] = "OPEN"
+            self.scan_list.append(temp_dict)
+        except (ConnectionRefusedError, OSError, asyncio.TimeoutError):
+            # # reason = {
+            # #     "ConnectionRe"
+            # # }
+            # temp_dict['port_state'] = "CLOSED"
+            pass
+        try:
+            temp_dict['service'] = socket.getservbyport(port)
+        except OSError:
+            temp_dict['service'] = 'unknown'
+
+    def execute(self):
+        with self._timer():
+            self._loop.run_until_complete(asyncio.wait(self._scan_tasks))
+        self._loop.run_until_complete(self._notify())
+        return self.scan_list
