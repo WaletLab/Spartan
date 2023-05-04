c8a762ea39967383da5976e2c8bd1006016c4868
dannyx-hub <daro322.dp@gmail.com>
2023-05-04 18:02:56 +0200

    visual changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import argparse
 from tabulate import tabulate
 from port_scan import Scanner
+from datetime import datetime
 
 debug = False
 if debug is not True:
@@ -17,6 +18,8 @@ parser.add_argument("--port", type=str, help="port to scan if all print -a if ra
 parser.add_argument("--mode", type=str, help="scan mode")
 parser.add_argument("--only_known_service", help="return only port with known services", action='store_true')
 parser.add_argument("--output", help="dump scan result to text file")
+parser.add_argument("--timeout",type=float, help="timeout for scanner")
+
 args = parser.parse_args()
 if len(sys.argv) == 1:
     # parser.print_usage()
@@ -25,28 +28,30 @@ if len(sys.argv) == 1:
 hostname = args.host
 port = None
 result = None
-os_detection = None
-# scan
-print("*" * 50)
-print(f"\tPort Scanner \nSpartan start to check ports on {hostname}")
-print("*" * 50)
+os_detection = False
+port_mode = ""
+mode = "no mode selected"
 if args.mode == "u":
-    print(" [*] udp mode activate [*] ")
+    mode = "udp scan"
     udp = True
 elif args.mode == "os":
-    print("[*] os detection activate [*]")
+    # print("[*] os detection activate [*]"))
+    mode = "os detection"
     os_detection = True
 if args.port == "a":
-    print(" [*] all ports [*] ")
+    # print(" [*] all ports [*] ")
+    port_mode = "all ports"
     lst = [p for p in range(1, 65355 + 1)]
     port = Port.split_port_lists(lst, 1100)
 elif args.port == "d":
+    port_mode = "top 1000 ports"
     lst = Port.top_1000
     port = [lst]
 
 else:
     if args.port.find("-") != -1:
         ports = args.port.split("-")
+        port_mode = "range between {}-{}".format(ports[0],ports[1])
         lst = [p for p in range(int(ports[0]), int(ports[1]))]
         if len(lst) > 1000:
             port = Port.split_port_lists(lst, 10)
@@ -55,17 +60,24 @@ else:
 
     else:
         port = [[int(args.port)]]
+        port_mode = "single port {}".format(args.port)
+print("=" * 50)
+print(f"Spartan start checks ports on "+color.BOLD+f"{hostname}"+color.END)
+print("Date: {} ".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+print(f"Scanner options: \n\t\t"+color.BOLD+"port: "+color.END+f" {port_mode}\n\t\t"+color.BOLD+f"scan mode: "+color.END+f"{mode}")
+print("=" * 50)
 if port:
-    if args.port == "a":
+    if args.port == "a" or args.mode == "os":
+        print("\n[?] "+color.YELLOW+"Warning"+color.END+" selected options may increase the scanning time [?]")
         timeout = 2.9
     else:
         timeout = 0.5
     timer = time.perf_counter()
-    result = Scanner(hostname, port, timeout)
+    result = Scanner(hostname, port, timeout,os_detection)
     result.execute()
     stop = time.perf_counter()
 if result.scan_list:
-    print(color.GREEN + "Result for {}:".format(hostname) + color.END)
+    print(color.GREEN + "\nResult for {}:".format(hostname) + color.END)
     print(f"found: {len(result.scan_list)}")
     if os_detection:
         header = [color.BOLD+'TYPE', 'PORT', 'STATUS', 'SERVICE', 'INFO'+color.END]
@@ -81,7 +93,7 @@ if result.scan_list:
             table_data = [[x['type'], x['port'], x['status'], x['service'], x['info']] for x in result.scan_list]
         else:
             table_data = [[x['type'], x['port'], x['status'], x['service']] for x in result.scan_list]
-    print(tabulate(table_data, headers=header, tablefmt="plain"))
+    print("\n"+tabulate(table_data, headers=header, tablefmt="plain"))
     if args.output:
         with open(args.output, "a") as outfile:
             outfile.writelines(tabulate(table_data, headers=header, tablefmt="plain"))
