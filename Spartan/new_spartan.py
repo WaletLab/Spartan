ff08044f9e840f053757e9fcbca4fa01985a3563
dannyx <daro322.dp@gmail.com>
2024-03-16 17:45:08 +0100

    [cli] init commit

---
+++ Spartan/new_spartan.py
@@ -0,0 +1,32 @@
+import typer
+from typing import Optional, List
+from art import tprint
+from lib.helpers.helpers import color
+from tabulate import tabulate
+from lib.port_scan import Scanner
+
+app = typer.Typer()
+def banner():
+    tprint("CyberSpartan")
+    print(color.ITALIC + "\t With great power comes great responsibility \n" + color.STOP_ITALIC)
+    print("v0.1.3 created by " + color.BOLD + "dannyx-hub\n" + color.END)
+def print_result(result):
+    table_data = []
+    header = [color.BOLD + 'TYPE', 'PORT', 'STATUS', 'SERVICE' + color.END]
+    if result:
+        for port, info in result.items():
+            table_data.append(['TCP', info['port'], info['state'], info['service']])
+        print("\n" + tabulate(table_data, headers=header, tablefmt="plain"))
+@app.command()
+def main(
+    host: str,
+    port: str,
+):
+    sc = Scanner(target=host, mode=port)
+    result = sc.start_scan()
+    if result:
+        print_result(result)
+
+if __name__ == "__main__":
+    banner()
+    typer.run(main)
