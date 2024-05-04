8ed157bb0c0d7a27da4a61920a3c0435ebca65d8
dannyx <daro322.dp@gmail.com>
2024-05-04 18:29:49 +0200

    helpers refactor

--- Spartan/lib/helpers/helpers.py
+++ Spartan/lib/helpers/helpers.py
@@ -22,26 +22,14 @@ def print_banner():
     print("[italic]created by WaletLab[/italic]\n")
 
 
+def print_scanner_options(date, mode, host, port, retry_timeout):
+    print("\n[bold blue]Scanner Options: [/bold blue]")
+    print(f"[bold]Date: [/bold] {date}\n[bold]Host: [/bold] {host}\n[bold]Mode: [/bold] {mode}\n[bold]Port: [/bold] {port}\n[bold]Retry timeout: [/bold] {retry_timeout}\n")
+
+
 def port_mode_parser(port):
     if port.find(":") != -1:
         port_range = port.split(":")
         return [x for x in range(int(port_range[0]), int(port_range[1])+1)]
     else:
         return port
-
-
-def params_table(params):
-    from rich.console import Console
-    from rich.table import Table
-
-    console = Console()
-    table = Table()
-
-    # Dodanie kolumn
-    table.add_column("param")
-    table.add_column("value")
-
-    for param, value in params:
-        table.add_row(param, value)
-
-    console.print(table)
